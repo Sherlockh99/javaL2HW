e66731636617be278a28110b7d1496766ec6679f
Sherlockh99 <sherlockh989@gmail.com>
2021-12-31 18:15:56 +0200

    HW 4

--- src/main/java/lesson4/HelloController.java
+++ src/main/java/lesson4/HelloController.java
@@ -1,14 +1,22 @@
 package lesson4;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public TextField textField;
+
+    @FXML
+    public TextArea textArea;
+
+    @FXML
+    public void clickBtnSendText(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
     }
 }
