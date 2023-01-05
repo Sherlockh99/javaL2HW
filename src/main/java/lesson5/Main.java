ee69d68875967daaecbbae837ef4b31ea4c59472
Sherlockh99 <sherlockh989@gmail.com>
2023-01-05 23:24:41 +0200

    task description 5 added

--- src/main/java/lesson5/Main.java
+++ src/main/java/lesson5/Main.java
@@ -1,5 +1,22 @@
 package lesson5;
 
+/**
+ * 1. You need to write two methods that do the following:
+ * 1) Create a one-dimensional long array
+ * 2) Fill this array with ones;
+ * 3) Track the execution time
+ * 4) Pass through the entire array and calculate a new value for each cell using the formula:
+ * arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)) ;
+ * 5) The end time of the System.currentTimeMillis() method is checked;
+ * 6) The running time is displayed in the console: System.out.println(System.currentTimeMillis() - a);
+ *
+ * The difference between the first method and the second:
+ * The first one simply runs through the array and calculates the values.
+ * The second splits the array into two arrays, calculates new values in two threads,
+ * and then glues these arrays back into one.
+ *
+ */
+
 public class Main {
     static final int SIZE = 10000000;
     static final int H = SIZE / 2;
