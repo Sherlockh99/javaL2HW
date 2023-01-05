b494d83e6f26edb26b662a8d001090a0f5b07d02
Sherlockh99 <sherlockh989@gmail.com>
2023-01-05 23:15:05 +0200

    translate lesson3

--- src/main/java/lesson3/Main.java
+++ src/main/java/lesson3/Main.java
@@ -3,6 +3,19 @@ package lesson3;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 1. Create an array with a set of words (10-20 words, there should be duplicates).
+ * Find and display a list of unique words that make up the array
+ * (duplicates are not counted).
+ * Count how many times each word occurs.
+ *
+ * 2. Write a simple PhoneBook class that stores a list of last names and phone numbers.
+ * You can add entries to this phone book using the add() method.
+ * Use the get() method to look up a phone number by last name.
+ * It should be noted that there can be several phones under one surname (in the case of namesakes),
+ * then when such a surname is requested, all phones should be displayed.
+ */
+
 public class Main {
     public static void main(String[] args) {
         words();
@@ -24,14 +37,14 @@ public class Main {
 
     public static void telNumbers(){
         TelephoneNumbers telephoneNumbers = new TelephoneNumbers();
-        telephoneNumbers.addNumber("Попова",149521);
-        telephoneNumbers.addNumber("Сидорова",44444);
-        telephoneNumbers.addNumber("Попова",456789);
-        telephoneNumbers.addNumber("Сидорова",55555);
-        telephoneNumbers.addNumber("Попова",999999);
-        telephoneNumbers.addNumber("Орлова",1111111);
-        telephoneNumbers.getNumbers("Попова");
-        telephoneNumbers.getNumbers("Сидорова");
-        telephoneNumbers.getNumbers("Орлова");
+        telephoneNumbers.addNumber("Popova",149521);
+        telephoneNumbers.addNumber("Sidorova",44444);
+        telephoneNumbers.addNumber("Popova",456789);
+        telephoneNumbers.addNumber("Sidorova",55555);
+        telephoneNumbers.addNumber("Popova",999999);
+        telephoneNumbers.addNumber("Orlova",1111111);
+        telephoneNumbers.getNumbers("Popova");
+        telephoneNumbers.getNumbers("Sidorova");
+        telephoneNumbers.getNumbers("Orlova");
     }
 }
