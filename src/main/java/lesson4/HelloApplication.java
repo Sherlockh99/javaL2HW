838c309f63d4f9ba248f8bc8a6b2b56a59d7b668
Sherlockh99 <sherlockh989@gmail.com>
2023-01-05 23:20:27 +0200

    task description added

--- src/main/java/lesson4/HelloApplication.java
+++ src/main/java/lesson4/HelloApplication.java
@@ -7,6 +7,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * 1. Create a window for the client part of the chat:
+ * a large text field for displaying correspondence in the center of the window.
+ * A one-line text field for entering messages and a button for sending messages on the bottom panel.
+ * The message should be sent either by pressing a button on the form,
+ * or by pressing the Enter button.
+ * When "sending" the message is thrown from the lower field to the central one.
+ */
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
